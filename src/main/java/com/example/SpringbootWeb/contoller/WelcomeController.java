9a20e4dac650a309a09e4691ca9c935bf8925d6e
DALTON LEYIAN <daltonleyian@gmail.com>
2024-01-30 10:59:23 +0300

    css modified

--- src/main/java/com/example/SpringbootWeb/contoller/WelcomeController.java
+++ src/main/java/com/example/SpringbootWeb/contoller/WelcomeController.java
@@ -1,13 +1,26 @@
 package com.example.SpringbootWeb.contoller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
+@RequestMapping("/welcome")
 public class WelcomeController {
     @GetMapping("/")
     public String showHome() {
+        return "home";
+    }
+
+    @PostMapping("process-form")
+    public String processHome(Model model, @RequestParam("email") String email) {
+
+        log.info("email : {}",email);
         return "home";
     }
 }
